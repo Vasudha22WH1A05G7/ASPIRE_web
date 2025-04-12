dd0ca9e9e2a1def81f038473a437a4b3a82681c1
Vasudha22WH1A05G7 <147139394+Vasudha22WH1A05G7@users.noreply.github.com>
2025-04-12 14:52:51 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        'gradient-x': 'gradient-x 8s ease infinite',
+        'fade-in': 'fade-in 1.5s ease-in forwards',
+        'fade-in-up': 'fade-in-up 1s ease forwards',
+      },
+      keyframes: {
+        'gradient-x': {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+        'fade-in-up': {
+          '0%': { opacity: 0, transform: 'translateY(40px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        'fade-in': {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+    },
+  },
+};
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
